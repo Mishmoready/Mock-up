5581267b71322d5f0e510254ab0717ab3e473bca
Mishmoready <moses@missionreadyhq.com>
2023-06-10 19:16:43 +1200

    Mission 2

---
+++ Mock-up and Practice/Week4/script.js
@@ -0,0 +1,10 @@
+console.log("difffz");
+document.body.innerHTML = "<h1>Good Morning World</h1>";
+document.body.style.background = "burlywood";
+function changeColour() {
+  if ((document.body.style.background = "burlywood")) {
+    document.body.style.background = "blue";
+  } else {
+    document.body.style.background = "burlywood";
+  }
+}
